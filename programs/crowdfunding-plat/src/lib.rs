447c9f89f251fb6d9131061fd6fbb5f5054360c0
zhubby <yanvalue@gmail.com>
2022-07-23 10:55:56 +0800

    new tic-tac-toe

--- programs/crowdfunding-plat/src/lib.rs
+++ programs/crowdfunding-plat/src/lib.rs
@@ -2,6 +2,8 @@ use anchor_lang::prelude::*;
 
 declare_id!("Fg6PaFpoGXkYsidMpWTK6W2BeZ7FEfcYkg476zPFsLnS");
 
+
+
 #[program]
 pub mod sol_crowdfunding_plat {
     use super::*;
---
+++ programs/tic-tac-toe/src/lib.rs
@@ -0,0 +1,46 @@
+use anchor_lang::prelude::*;
+use num_derive::*;
+use num_traits::*;
+
+declare_id!("Fg6PaFpoGXkYsidMpWTK6W2BeZ7FEfcYkg476zPFsLnS");
+
+#[program]
+pub mod tic_tac_toe {
+    use super::*;
+
+    pub fn initialize(ctx: Context<Initialize>) -> Result<()> {
+        Ok(())
+    }
+}
+
+
+
+#[derive(
+    AnchorSerialize,
+    AnchorDeserialize,
+    FromPrimitive,
+    ToPrimitive,
+    Copy,
+    Clone,
+    PartialEq,
+    Eq
+)]
+pub enum Sign {
+    X,
+    O,
+}
+
+#[derive(AnchorSerialize, AnchorDeserialize, Clone, PartialEq, Eq)]
+pub enum GameState {
+    Active,
+    Tie,
+    Won { winner: Pubkey },
+}
+
+#[account]
+pub struct Game {
+    players: [Pubkey; 2],          // (32 * 2)
+    turn: u8,                      // 1
+    board: [[Option<Sign>; 3]; 3], // 9 * (1 + 1) = 18
+    state: GameState,              // 32 + 1
+}
